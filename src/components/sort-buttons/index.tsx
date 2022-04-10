eeec2fc40627da9f4142852e893029dd81f6f665
Sergey Tokarev <sergey.zoomaster@gmail.com>
2022-04-11 05:50:28 +0700

    sort buttoms optimizes

--- src/components/sort-buttons/index.tsx
+++ src/components/sort-buttons/index.tsx
@@ -1,20 +1,37 @@
-import React  from 'react';
+import React from 'react';
 import styles from './sort-buttons.module.css';
+import { Services } from '../../services';
+import { ActionTicketType, TicketSort, useDispatch, useTicketState } from '../../store';
 
-export interface ISortButtonsProps {
-  selected: number;
-  onSelect: (i: number) => void;
-  buttons: string[];
+interface ISortButtonData {
+  label: string;
+  order: TicketSort;
 }
 
+const buttons: ISortButtonData[] = [
+  { label: Services.strings.ticketSortCheapest, order: TicketSort.CHEAPEST },
+  { label: Services.strings.ticketSortFastest, order: TicketSort.FASTEST },
+  { label: Services.strings.ticketSortOptimal, order: TicketSort.OPTIMAL },
+];
 
-export const SortButtons: React.FC<ISortButtonsProps> = ({ buttons, selected, onSelect }) => {
+export const SortButtons: React.FC = () => {
 
+  const { sortOrder } = useTicketState();
+  const dispatch = useDispatch();
+  console.log('sortOrder', sortOrder);
+
+  const onSelect = (i: number) => {
+    dispatch({
+      type: ActionTicketType.SORT,
+      order: buttons[i]?.order ?? TicketSort.FASTEST,
+    });
+  };
+  
   return (
     <div className={`block ${styles.group} `}>
-      {buttons.map((text, i) => (
-        <button key={i} className={selected === i ? styles.selected : ''}
-                onClick={() => onSelect(i)}>{text}</button>))}
+      {buttons.map(({ order, label }, i) => (
+        <button key={i} className={order === sortOrder ? styles.selected : ''}
+                onClick={() => onSelect(i)}>{label}</button>))}
     </div>
   );
 };
--- src/pages/tickets/index.tsx
+++ src/pages/tickets/index.tsx
@@ -1,37 +1,9 @@
 import React from 'react';
 import 'common.css';
-import { Services } from 'services';
-import { ActionTicketType, TicketSort, useDispatch, useTicketState } from 'store';
 import { CompanySelector, Logo, SortButtons, Ticket, TransferChecks } from 'components';
 import styles from './tickets.module.css';
 
-interface ISortButtonData {
-  label: string;
-  order: TicketSort;
-}
-
-const buttons: ISortButtonData[] = [
-  { label: Services.strings.ticketSortCheapest, order: TicketSort.CHEAPEST },
-  { label: Services.strings.ticketSortFastest, order: TicketSort.FASTEST },
-  { label: Services.strings.ticketSortOptimal, order: TicketSort.OPTIMAL },
-];
-
 export const Tickets: React.FC = () => {
-
-  const { tickets, sortOrder } = useTicketState();
-  const dispatch = useDispatch();
-
-  console.log('sortOrder', sortOrder);
-
-  const onSortSelect = (i: number) => {
-    dispatch({
-      type: ActionTicketType.SORT,
-      order: buttons[i]?.order ?? TicketSort.FASTEST,
-    });
-  };
-
-  const getSelectedButtonIndex: number = buttons.findIndex(b => b.order === sortOrder) || 0;
-
   return (<div className={`page`}>
     <div className={`pageHeader ${styles.logo}`}>
       <Logo />
@@ -42,7 +14,7 @@ export const Tickets: React.FC = () => {
         <CompanySelector />
       </div>
       <div className={`pageColumn pageMain`}>
-        <SortButtons buttons={buttons.map(b => b.label)} selected={getSelectedButtonIndex} onSelect={onSortSelect} />
+        <SortButtons />
         <Ticket />
         <Ticket />
         <Ticket />
