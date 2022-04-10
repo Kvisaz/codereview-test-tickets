2f2afe730912f848bbfbfdc7c013d20174d0a579
Sergey Tokarev <sergey.zoomaster@gmail.com>
2022-04-11 03:12:35 +0700

    sort-buttons state

--- src/components/sort-buttons/index.tsx
+++ src/components/sort-buttons/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './sort-buttons.module.css';
 
 export interface ISortButtonsProps {
 
 }
 
+const buttons = [
+  { text: 'Самый дешевый' },
+  { text: 'Самый быстрый' },
+  { text: 'Оптимальный' },
+];
+
 export const SortButtons: React.FC<ISortButtonsProps> = () => {
+
+  const [selectedButton, setSelectedButton] = useState(0);
+
+  const onClick = (i: number) => {
+    console.log(`${i}`);
+    setSelectedButton(i);
+  };
+
+
   return (
     <div className={`block ${styles.group} `}>
-      <button>Самый дешевый</button>
-      <button>Самый быстрый</button>
-      <button>Оптимальный</button>
+      {buttons.map((props, i) => (
+        <button key={i} className={selectedButton === i ? styles.selected : ''}
+                onClick={() => onClick(i)}>{props.text}</button>))}
     </div>
   );
 };
