df328ae2f82e251a20681e7966712a8519ee0d3e
Sergey Tokarev <sergey.zoomaster@gmail.com>
2022-04-11 09:14:24 +0700

    tickets sorting

--- src/components/tickets/index.tsx
+++ src/components/tickets/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { RequestState, useApiState, useTicketState } from 'store';
+import { ISegment, ITicket, RequestState, TicketSort, useApiState, useTicketState } from 'store';
 import { Ticket, TicketSkeleton } from '../ticket';
 
 export const Tickets: React.FC = () => {
-  const { tickets, nextTicketAmount } = useTicketState();
+  const { tickets, nextTicketAmount, segments, sortOrder } = useTicketState();
   const { ticketsRequestState, error } = useApiState();
-
-  console.log('ticketsRequestState', ticketsRequestState);
 
   if (ticketsRequestState === RequestState.REJECTED) {
     return (
@@ -16,14 +14,50 @@ export const Tickets: React.FC = () => {
     );
   }
 
-  const skeletonsAmount = ticketsRequestState === RequestState.PENDING || ticketsRequestState === RequestState.NOT_SETTED ? nextTicketAmount : 0;
+  const ticketArr = Object.values(tickets);
+  const sortedTickets = sortTickets(ticketArr, sortOrder, segments);
   
+  const skeletonsAmount = ticketsRequestState === RequestState.PENDING || ticketsRequestState === RequestState.NOT_SETTED ? nextTicketAmount : 0;
+
   return (
     <>
-      {Object.values(tickets).map(ticket => (<Ticket key={ticket.id} ticket={ticket} />))}
+      {sortedTickets.map(ticket => (<Ticket key={ticket.id} ticket={ticket} />))}
       {
         skeletonsAmount > 0 && Array.from(new Array(skeletonsAmount)).map((_, i) => (<TicketSkeleton key={i} />))
       }
     </>
   );
 };
+
+function sortTickets(tickets: ITicket[], order: TicketSort, segments: Record<string, ISegment>): ITicket[] {
+  switch (order) {
+    case TicketSort.CHEAPEST:
+      return getCheapest(tickets);
+    case TicketSort.FASTEST:
+      return getFastest(tickets, segments);
+    case TicketSort.OPTIMAL:
+      return getOptimal(tickets, segments);
+  }
+}
+
+function getCheapest(tickets: ITicket[]): ITicket[] {
+  return tickets.sort((a, b) => a.price - b.price);
+}
+
+function getFastest(tickets: ITicket[], segments: Record<string, ISegment>): ITicket[] {
+  return tickets.sort((a, b) => getTicketTime(a, segments) - getTicketTime(b, segments));
+}
+
+function getOptimal(tickets: ITicket[], segments: Record<string, ISegment>): ITicket[] {
+  return tickets.sort((a, b) => getTicketPriceTime(a, segments) - getTicketPriceTime(b, segments));
+}
+
+function getTicketTime(ticket: ITicket, segments: Record<string, ISegment>): number {
+  const segArr = ticket.segments.map(id => segments[id]).sort((a, b) => a.dateStart - b.dateStart);
+  console.log('segArr', segArr);
+  return segArr[segArr.length - 1].dateEnd - segArr[0].dateStart;
+}
+
+function getTicketPriceTime(ticket: ITicket, segments: Record<string, ISegment>): number {
+  return getTicketTime(ticket, segments) * ticket.price;
+}
