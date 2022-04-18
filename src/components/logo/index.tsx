decf90bc314cc12a4d83bad932d77b9fc32c2c91
Sergey Tokarev <sergey.zoomaster@gmail.com>
2022-04-18 22:35:06 +0700

    movsumlu - removed void interface

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,10 +3,7 @@ import { Services } from 'services';
 import IMAGE from './logo.svg';
 import styles from './logo.module.css';
 
-interface IProps {
-}
-
-export const Logo: React.FC<IProps> = () => {
+export const Logo: React.FC = () => {
   return (
     <a href='/'>
       <img src={IMAGE} alt={Services.strings.logoTitle} className={styles.logo} />
